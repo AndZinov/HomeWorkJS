990de396913cca376b454289c75d5a4dd3d2047d
Andrey Zinovev <andreyzinovv@gmail.com>
2022-12-26 16:30:39 +0300

    сс

--- Additionally Tasks/Week 7/index.js
+++ Additionally Tasks/Week 7/index.js
@@ -2,9 +2,7 @@
 const bodyElement = document.querySelector('body')
 
 const createElementHTML = (body, email) => {
-    const divElement = document.createElement('div');
-    divElement.classList = 'post__comments';
-
+    
     const divElement2 = document.createElement('div');
     divElement2.classList = 'post__comment';
 
@@ -16,11 +14,10 @@ const createElementHTML = (body, email) => {
     spanElement2.classList = 'post__comments-text';
     spanElement2.textContent = body;
 
-    divElement.append(divElement2);
-    divElement2.append(spanElement);
-    divElement2.append(spanElement2);
     
-    return divElement
+    divElement2.append(spanElement, spanElement2);
+    
+    return divElement2
 }
 const createTitle = (title,body) => {
         const divElement = document.createElement('div');
@@ -29,15 +26,19 @@ const createTitle = (title,body) => {
         const h1Element = document.createElement('h1');
         h1Element.classList = 'post__title'
         h1Element.textContent = title
-        divElement.append(h1Element)
+       
         const pElement = document.createElement('p');
         pElement.classList = 'post__text';
         pElement.textContent = body;
-        divElement.append(pElement)
         const bElement = document.createElement('b');
         bElement.classList = 'post__comments-text';
         bElement.textContent = 'Комментарии';
-        divElement.append(bElement)
+
+        const divElements = document.createElement('div');
+        divElements.classList = 'post__comments';
+
+        divElement.append(h1Element, pElement, bElement, divElements)
+
 
         return divElement
 }
@@ -52,7 +53,7 @@ const renderPost = async (postId) => {
         const {title, body} = dataResponse;
         const titleHTML = createTitle(title,body);
         bodyElement.append(titleHTML)
-        const postTitle = document.querySelector('.post')
+        const postTitle = document.querySelector('.post__comments')
         const comment = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
         if(!comment.ok){
             throw new Error('Ошибка получения комментариев')
@@ -69,3 +70,80 @@ const renderPost = async (postId) => {
 }
 
 renderPost(1)
+
+
+//Решение от школы
+
+/*
+const createComment = (author, text) => {
+  const comment = document.createElement("div");
+  comment.className = "post-comment";
+
+  const commentAuthor = document.createElement("span");
+  commentAuthor.className = "post-comment__author";
+  commentAuthor.innerText = author;
+
+  const commentText = document.createElement("span");
+  commentText.className = "post-comment__text";
+  commentText.innerText = text;
+
+  comment.append(commentAuthor, commentText);
+
+  return comment;
+};
+
+const createPost = (postName, text, comments) => {
+  const post = document.createElement("div");
+  post.id = "post";
+  post.className = "post";
+
+  const postTitle = document.createElement("h1");
+  postTitle.className = "post__title";
+  postTitle.innerText = postName;
+
+  const postText = document.createElement("p");
+  postText.className = "post__text";
+  postText.innerText = text;
+
+  const commentsText = document.createElement("b");
+  commentsText.className = "post__comments-text";
+  commentsText.innerText = "Комментарии";
+
+  const commentsBlock = document.createElement("div");
+  commentsBlock.className = "post__comments";
+
+  comments.forEach((comment) => {
+    const commentHTML = createComment(comment.email, comment.body);
+    commentsBlock.append(commentHTML);
+  });
+
+  post.append(postTitle, postText, commentsText, commentsBlock);
+
+  return post;
+};
+
+const COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
+const renderPost = (postId) => {
+  fetch(`${POSTS_URL}/${postId}`)
+    .then((response) => response.json())
+    .then((post) => {
+      fetch(`${COMMENTS_URL}?postId=${post.id}`)
+        .then((response) => response.json())
+        .then((comments) => {
+          const postHTML = createPost(post.title, post.body, comments);
+          document.body.append(postHTML);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+renderPost(1);
+
+*/ 
---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+    input: './Lesson-17 Webpack,Rollup/Lesson material/index.js',
+    output: {
+      file: './build/bundle.js',
+      format: 'cjs'
+    }
+  };
