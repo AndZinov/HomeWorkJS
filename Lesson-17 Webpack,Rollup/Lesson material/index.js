1ddfa714a5544e9e3af188545c0c08ed3e9ccc32
Andrey Zinovev <andreyzinovv@gmail.com>
2022-12-28 12:38:15 +0300

    lesson 17

--- Lesson-17 Webpack,Rollup/Lesson material/index.js
+++ Lesson-17 Webpack,Rollup/Lesson material/index.js
@@ -1,1 +1,16 @@
+import "./index.css";
+import JS_IMAGE from './asset/js.png';
+
 console.log('Hello world')
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.className = 'js-image';
+jsImageHTML.src = JS_IMAGE;
+
+
+document.body.append(jsImageHTML);
+
+const array = [1, 2, 3].map(n => n + 1);
+console.log(array);
+
+
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,30 @@
+import { babel } from '@rollup/plugin-babel';
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload'
+
+
 export default {
     input: './Lesson-17 Webpack,Rollup/Lesson material/index.js',
     output: {
-      file: './build/bundle.js',
+      file: './/Lesson-17 Webpack,Rollup/Lesson material/build/bundle.js',
       format: 'cjs'
-    }
+    },
+    plugins: [
+      babel({
+         babelHelpers: 'bundled',
+         presets: ["@babel/env"]
+         }),
+      styles(),
+      image({
+        limit: 100000,
+      }),
+      serve({
+        open: true,
+        contentBase: './Lesson-17 Webpack,Rollup/Lesson material',
+        port: 8001,
+      }),
+      livereload(),
+    ]
   };
